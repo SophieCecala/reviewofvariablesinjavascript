dfb692a120d8ef948c96de12cb4ad086f59c6db5
Natalya Myunster <natalyamyunster@yahoo.com>
2021-04-01 13:33:37 +0200

    Initial commit

---
+++ script.js
@@ -0,0 +1,81 @@
+// let num = 123;
+// alert(num);
+//
+// let a = 1;
+// let b = 2;
+// let c = 3;
+//
+//
+//
+// let d = 1, e = 2, f = 3;
+//
+//
+// let g, h, i;
+//
+// g = 1;
+// h = 2;
+// i = 3;
+//
+//
+// let l;
+// l = 10;
+// alert(l);
+//
+// l = 20;
+// alert(l);
+
+// alert(`result: ${354*565}`)
+
+// let a = '10';
+// let b = '20';
+// alert(Number(a) + Number(b));
+
+
+// let a = '2';
+// let b = '3';
+//  alert(Number('2') + Number('3'));
+
+// let a = 2;
+// let b = '3';
+// alert(2 + Number('3'));
+
+
+// let a = '2';
+// let b = '3';
+// alert('2' + Number('3'));
+
+
+// let a = '2';
+// let b = '3';
+// alert(+a + +b);
+
+
+// let num = parseInt('5px');
+// let numb = parseInt('6px');
+// alert(num + numb);
+
+
+// let num = parseFloat('5.5px');
+// let numb = parseFloat('6.25px');
+// alert(num + numb);
+
+
+// let num = parseFloat('5.5px');
+// let numb = parseFloat('6.25px');
+// let a = "px";
+// alert(num + numb + a);
+
+
+// let a = 23;
+// let b = 46;
+// alert(String(a) + String(b));
+
+
+// let num = 6789367;
+// alert(String(num).length);
+
+
+
+let num = 6789;
+let numb = 3476;
+alert(String(num).length + String(numb).length);
